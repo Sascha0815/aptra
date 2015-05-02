1f80df73ce797630dbc073922c4f1c07bfc244fc
Fabian <fabian.gierer@web.de>
2015-05-02 10:42:06 +0200

    Kleine Änderung

--- SAP_APTRA/src/git_aptra/Login/Login.java
+++ SAP_APTRA/src/git_aptra/Login/Login.java
@@ -2,17 +2,14 @@ package git_aptra.Login;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import com.sun.javafx.scene.EnteredExitedHandler;
 
 public class Login {
 	static int entitlement = 0;
 	
 	public static int login(String username, String password) {
-		System.out.println("SELECT entitlement from employee where loginName = '" +  username + "' AND password = '" +  password +"'");
 		try {
 			Connection con = DriverManager.getConnection(
 					"jdbc:mysql://185.28.20.242:3306/u474396146_db",
