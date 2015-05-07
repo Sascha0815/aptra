04422064177de51c9a464b7ba49519c36b9c4447
ToasterIowa13 <vollmer.johannes@web.de>
2015-05-07 18:22:40 +0200

    Login Dialog überarbeitet

--- SAP_APTRA/src/git_aptra/Login/DialogLogin.java
+++ SAP_APTRA/src/git_aptra/Login/DialogLogin.java
@@ -3,12 +3,11 @@ package git_aptra.Login;
 import git_aptra.Oberflaeche;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,52 +18,56 @@ import javax.swing.JTextField;
 public class DialogLogin extends JFrame {
 	private static final long serialVersionUID = 1L;
 	public JFrame frameLogin = new JFrame();
-	private JPanel panelLogin = new JPanel();
-	private JLabel labelInstruction = new JLabel(
-			"Bitte geben sie ihre Zugangsdaten ein.");
+	private JPanel panelLogin = new JPanel(new GridBagLayout());
+	private JLabel labelInstruction = new JLabel("Bitte geben sie ihre Zugangsdaten ein.");
 	private JLabel labelUsername = new JLabel("Benutzername:");
 	private JLabel labelPassword = new JLabel("Passwort:");
-	private JLabel labelWarning = new JLabel("Die eingegebenen Zugangsdaten sind nicht korrekt!");
-	private JTextField fieldUsername = new JTextField();
-	private JPasswordField fieldPassword = new JPasswordField();
+	private JLabel labelWarning = new JLabel("Die Zugangsdaten sind nicht korrekt!");
+	private JTextField fieldUsername = new JTextField(20);
+	private JPasswordField fieldPassword = new JPasswordField(20);
 	private JButton buttonLogin = new JButton("Einloggen");
 	private Font fontLogin = new Font("Arial", Font.BOLD, 12);
 	private Font fontInstruction = new Font("Arial", Font.BOLD, 14);
 	private String username;
 	private String password;
 	private int entitlement = 0;
-	
+	private GridBagConstraints gbc = new GridBagConstraints();
 
 	public DialogLogin() {
-		frameLogin.setSize(400, 250);
+		frameLogin.setSize(300, 230);
 		frameLogin.setTitle("Login");
 		frameLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frameLogin.setResizable(false);
+		frameLogin.setResizable(true);
 		frameLogin.setLocationRelativeTo(null);
 		frameLogin.add(panelLogin);
-		
-		panelLogin.setLayout(new BoxLayout(panelLogin, BoxLayout.Y_AXIS));
-		panelLogin.setBackground(Color.LIGHT_GRAY);
-		panelLogin.add(Box.createRigidArea(new Dimension(0, 10)));
-		panelLogin.add(labelInstruction);
-		labelInstruction.setFont(fontInstruction);
-		panelLogin.add(Box.createRigidArea(new Dimension(0, 15)));
-		panelLogin.add(labelUsername);
-		labelUsername.setFont(fontLogin);
-		panelLogin.add(Box.createRigidArea(new Dimension(0, 10)));
-		panelLogin.add(fieldUsername);
-		panelLogin.add(Box.createRigidArea(new Dimension(0, 10)));
-		panelLogin.add(labelPassword);
-		labelPassword.setFont(fontLogin);
-		panelLogin.add(Box.createRigidArea(new Dimension(0, 10)));
-		panelLogin.add(fieldPassword);
-		panelLogin.add(Box.createRigidArea(new Dimension(0, 10)));
-		panelLogin.add(buttonLogin);
-		panelLogin.add(Box.createRigidArea(new Dimension(0, 15)));
-		panelLogin.add(labelWarning);
-		labelWarning.setVisible(false);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 0;
+	    gbc.gridy = 0;
+	    gbc.weighty = 1;
+	    labelInstruction.setFont(fontInstruction);
+	    panelLogin.add(labelInstruction,gbc);
+	    gbc.gridx = 0;
+	    gbc.gridy = 1;
+	    panelLogin.add(labelUsername,gbc);
+	    gbc.gridx = 0;
+	    gbc.gridy = 2;
+	    panelLogin.add(fieldUsername,gbc);
+	    gbc.gridx = 0;
+	    gbc.gridy = 3;
+	    panelLogin.add(labelPassword,gbc);
+	    gbc.gridx = 0;
+	    gbc.gridy = 4;
+	    panelLogin.add(fieldPassword,gbc);
+	    gbc.gridx = 0;
+	    gbc.gridy = 5;
+	    gbc.weighty = 1;
+	    panelLogin.add(buttonLogin,gbc);
+	    labelWarning.setVisible(false);
 		labelWarning.setFont(fontLogin);
 		labelWarning.setForeground(Color.red);
+	    gbc.gridx = 0;
+	    gbc.gridy = 6;
+	    panelLogin.add(labelWarning,gbc);
 			
 		buttonLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
@@ -87,8 +90,6 @@ public class DialogLogin extends JFrame {
 				}
 			}
 		});
-		
-		
 		frameLogin.setVisible(true);
 	}	
 }
