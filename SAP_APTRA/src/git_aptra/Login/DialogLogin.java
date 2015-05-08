bde7b8328d56caa0ab8a8da5ec0e90945710c939
ToasterIowa13 <vollmer.johannes@web.de>
2015-05-08 16:13:02 +0200

    Login Dialog überarbeitet

--- SAP_APTRA/src/git_aptra/Login/DialogLogin.java
+++ SAP_APTRA/src/git_aptra/Login/DialogLogin.java
@@ -8,7 +8,9 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,23 +18,35 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class DialogLogin extends JFrame {
+	
 	private static final long serialVersionUID = 1L;
+	
 	public JFrame frameLogin = new JFrame();
+	
 	private JPanel panelLogin = new JPanel(new GridBagLayout());
+	
 	private JLabel labelInstruction = new JLabel("Bitte geben sie ihre Zugangsdaten ein.");
 	private JLabel labelUsername = new JLabel("Benutzername:");
 	private JLabel labelPassword = new JLabel("Passwort:");
 	private JLabel labelWarning = new JLabel("Die Zugangsdaten sind nicht korrekt!");
+	
 	private JTextField fieldUsername = new JTextField(20);
 	private JPasswordField fieldPassword = new JPasswordField(20);
+	
 	private JButton buttonLogin = new JButton("Einloggen");
+	private JButton buttonAbort = new JButton("Abbrechen");
+	
 	private Font fontLogin = new Font("Arial", Font.BOLD, 12);
 	private Font fontInstruction = new Font("Arial", Font.BOLD, 14);
+	
+	private JCheckBox checkRemember = new JCheckBox("Login-Daten merken?");
+	
+	private GridBagConstraints gbc = new GridBagConstraints();
+	
 	private String username;
 	private String password;
 	private int entitlement = 0;
-	private GridBagConstraints gbc = new GridBagConstraints();
-
+	
 	public DialogLogin() {
 		frameLogin.setSize(300, 230);
 		frameLogin.setTitle("Login");
@@ -41,7 +55,6 @@ public class DialogLogin extends JFrame {
 		frameLogin.setLocationRelativeTo(null);
 		frameLogin.getRootPane().setDefaultButton(buttonLogin);
 		frameLogin.add(panelLogin);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.gridx = 0;
 	    gbc.gridy = 0;
 	    gbc.weighty = 1;
@@ -49,6 +62,7 @@ public class DialogLogin extends JFrame {
 	    panelLogin.add(labelInstruction,gbc);
 	    gbc.gridx = 0;
 	    gbc.gridy = 1;
+	    gbc.fill = GridBagConstraints.HORIZONTAL;
 	    panelLogin.add(labelUsername,gbc);
 	    gbc.gridx = 0;
 	    gbc.gridy = 2;
@@ -61,13 +75,22 @@ public class DialogLogin extends JFrame {
 	    panelLogin.add(fieldPassword,gbc);
 	    gbc.gridx = 0;
 	    gbc.gridy = 5;
-	    gbc.weighty = 1;
+	    panelLogin.add(checkRemember,gbc);
+	    gbc.gridx = 0;
+	    gbc.gridy = 6;
+	    gbc.anchor = GridBagConstraints.WEST;
+	    gbc.fill = GridBagConstraints.NONE;
 	    panelLogin.add(buttonLogin,gbc);
+	    gbc.gridx = 0;
+	    gbc.gridy = 6;
+	    gbc.anchor = GridBagConstraints.EAST;
+	    panelLogin.add(buttonAbort,gbc);
 	    labelWarning.setVisible(false);
 		labelWarning.setFont(fontLogin);
 		labelWarning.setForeground(Color.red);
 	    gbc.gridx = 0;
-	    gbc.gridy = 6;
+	    gbc.gridy = 8;
+	    gbc.fill = GridBagConstraints.HORIZONTAL;
 	    panelLogin.add(labelWarning,gbc);
 			
 		buttonLogin.addActionListener(new ActionListener() {
@@ -91,6 +114,14 @@ public class DialogLogin extends JFrame {
 				}
 			}
 		});
+		
+		buttonAbort.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				frameLogin.dispose();
+			}
+		});
+		
 		frameLogin.setVisible(true);
 	}	
+	
 }
