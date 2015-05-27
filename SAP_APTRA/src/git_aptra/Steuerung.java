d2085004afa89fd94540d43c66a57b49a445d063
ToasterIowa13 <vollmer.johannes@web.de>
2015-05-27 09:57:23 +0200

    Aluminium Look

--- SAP_APTRA/src/git_aptra/Steuerung.java
+++ SAP_APTRA/src/git_aptra/Steuerung.java
@@ -1,12 +1,28 @@
 package git_aptra;
 
+import javax.swing.UnsupportedLookAndFeelException;
+
 import git_aptra.Login.DialogLogin;
 
 public class Steuerung {
 	//METHODE: main
 	public static void main(String[] args) {	
+		try {
+			Design.aluminiumLook();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		new DialogLogin();
-		//new Oberflaeche();
 	}	
 }
 
