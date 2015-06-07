48970ab7f16b5cc74bc8ceff4eca01a2e84e42fd
Fabian <fabian.gierer@web.de>
2015-06-07 17:42:32 +0200

    Designoptimierung Übersicht

--- SAP_APTRA/src/git_aptra/MenuBar/MenuBarPanelOverview.java
+++ SAP_APTRA/src/git_aptra/MenuBar/MenuBarPanelOverview.java
@@ -1,18 +1,22 @@
 package git_aptra.MenuBar;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+
 import git_aptra.Oberflaeche;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -46,15 +50,19 @@ public class MenuBarPanelOverview {
 	private static JPanel panelVacancy = new JPanel();
 	private static JPanel panelManagement = new JPanel();
 	private static JPanel panelMeeting = new JPanel();
-
+	private static Font fontButton = new Font("Calibri", Font.PLAIN, 16);
+	
+	
 	// SWING: Übersicht-Panel
 	public static void addPanelOverview() {
 		panelOverview.setLayout(new MigLayout("", "[]1%![]1%![]1%![]", "[]"));
 		Border border = new LineBorder(Color.gray, 2);
-		panelApplicant.setLayout(new MigLayout("", "[90%!]","[]5%![]5%![]5%![]"));
-		panelVacancy.setLayout(new MigLayout("", "[90%!]","[]5%![]5%![]5%![]"));
-		panelManagement.setLayout(new MigLayout("", "[90%!]","[]5%![]5%![]5%![]"));
-		panelMeeting.setLayout(new MigLayout("", "[90%!]","[]5%![]5%![]5%![]"));
+		Border line =  new MatteBorder(0,0,2,0, Color.gray);
+		
+		panelApplicant.setLayout(new MigLayout("", "[90%!]","15px![]5%![]5%![]5%![]"));
+		panelVacancy.setLayout(new MigLayout("", "[90%!]","15px![]5%![]5%![]5%![]"));
+		panelManagement.setLayout(new MigLayout("", "[90%!]","15px![]5%![]5%![]5%![]"));
+		panelMeeting.setLayout(new MigLayout("", "[90%!]","15px![]5%![]5%![]5%![]"));
 		
 		panelOverview.add(panelApplicant, "cell 0 0, w 24%!, h 98%!");
 		panelOverview.add(panelVacancy, "cell 1 0,  w 24%!, h 98%!");
@@ -67,10 +75,10 @@ public class MenuBarPanelOverview {
 		
 		
 		buttonNullApplicant.setToolTipText("Bewerber");
-		panelApplicant.add(buttonNullApplicant, "cell 0 0, alignx center");
+		panelApplicant.add(buttonNullApplicant, "cell 0 0, alignx center, w 95%!, h 170px");
+		buttonNullApplicant.setBorder(line);
 		buttonNullApplicant.setOpaque(false);
 		buttonNullApplicant.setContentAreaFilled(false);
-		buttonNullApplicant.setBorder(null);
 		try {
 			Image add = ImageIO.read(MenuBarPanelApplicant.class
 					.getResource("resources/applicant_null.png"));   
@@ -82,18 +90,25 @@ public class MenuBarPanelOverview {
 				Oberflaeche.tabBar.setSelectedIndex(1);
 			}
 		});
-		panelApplicant.add(buttonAddApplicant, "cell 0 1, alignx center");
-		panelApplicant.add(buttonEditApplicant, "cell 0 2, alignx center");
-		panelApplicant.add(buttonSearchApplicant, "cell 0 3, alignx center");
-		panelApplicant.add(buttonInfoApplicant, "cell 0 4, alignx center");
+		panelApplicant.add(buttonAddApplicant, "cell 0 1, alignx center, w 95%!, h 5%!");
+		panelApplicant.add(buttonEditApplicant, "cell 0 2, alignx center, w 95%!, h 5%!");
+		panelApplicant.add(buttonSearchApplicant, "cell 0 3, alignx center, w 95%!, h 5%!");
+		panelApplicant.add(buttonInfoApplicant, "cell 0 4, alignx center, w 95%!, h 5%!");
 		
-		
+		buttonAddApplicant.setBorder(border);
+		buttonAddApplicant.setFont(fontButton);
+		buttonEditApplicant.setBorder(border);
+		buttonEditApplicant.setFont(fontButton);
+		buttonSearchApplicant.setBorder(border);
+		buttonSearchApplicant.setFont(fontButton);
+		buttonInfoApplicant.setBorder(border);
+		buttonInfoApplicant.setFont(fontButton);
 		
 		buttonNullVacancy.setToolTipText("Arbeitsstellen");
-		panelVacancy.add(buttonNullVacancy, "cell 0 0, alignx center");
+		buttonNullVacancy.setBorder(line);
+		panelVacancy.add(buttonNullVacancy, "cell 0 0, alignx center, w 95%!, h 170px");
 		buttonNullVacancy.setOpaque(false);
 		buttonNullVacancy.setContentAreaFilled(false);
-		buttonNullVacancy.setBorder(null);
 		try {
 			Image add = ImageIO.read(MenuBarPanelApplicant.class
 					.getResource("resources/vacancy_null.png"));   
@@ -106,18 +121,25 @@ public class MenuBarPanelOverview {
 			}
 		});
 		
-		panelVacancy.add(buttonAddVacancy, "cell 0 1, alignx center");
-		panelVacancy.add(buttonEditVacancy, "cell 0 2, alignx center");
-		panelVacancy.add(buttonSearchVacancy, "cell 0 3, alignx center");
-		panelVacancy.add(buttonInfoVacancy, "cell 0 4, alignx center");
+		panelVacancy.add(buttonAddVacancy, "cell 0 1, alignx center, w 95%!, h 5%!");
+		panelVacancy.add(buttonEditVacancy, "cell 0 2, alignx center, w 95%!, h 5%!");
+		panelVacancy.add(buttonSearchVacancy, "cell 0 3, alignx center, w 95%!, h 5%!");
+		panelVacancy.add(buttonInfoVacancy, "cell 0 4, alignx center, w 95%!, h 5%!");	
 		
-		
+		buttonAddVacancy.setBorder(border);
+		buttonAddVacancy.setFont(fontButton);
+		buttonEditVacancy.setBorder(border);
+		buttonEditVacancy.setFont(fontButton);
+		buttonSearchVacancy.setBorder(border);
+		buttonSearchVacancy.setFont(fontButton);
+		buttonInfoVacancy.setBorder(border);
+		buttonInfoVacancy.setFont(fontButton);
 		
 		buttonNullManagement.setToolTipText("Stellenmanagement");
-		panelManagement.add(buttonNullManagement, "cell 0 0, alignx center");
+		buttonNullManagement.setBorder(line);
+		panelManagement.add(buttonNullManagement, "cell 0 0, alignx center, w 95%!, h 170px");
 		buttonNullManagement.setOpaque(false);
 		buttonNullManagement.setContentAreaFilled(false);
-		buttonNullManagement.setBorder(null);
 		try {
 			Image add = ImageIO.read(MenuBarPanelApplicant.class
 					.getResource("resources/management_null.png"));   
@@ -129,17 +151,19 @@ public class MenuBarPanelOverview {
 				Oberflaeche.tabBar.setSelectedIndex(3);
 			}
 		});
-		panelManagement.add(buttonEditManagement, "cell 0 1, alignx center");
-		panelManagement.add(buttonSearchManagement, "cell 0 2, alignx center");
+		panelManagement.add(buttonEditManagement, "cell 0 1, alignx center, w 95%!, h 5%!");
+		panelManagement.add(buttonSearchManagement, "cell 0 2, alignx center, w 95%!, h 5%!");
 		
-		
-		
+		buttonEditManagement.setBorder(border);
+		buttonEditManagement.setFont(fontButton);
+		buttonSearchManagement.setBorder(border);
+		buttonSearchManagement.setFont(fontButton);
 			
 		buttonNullMeeting.setToolTipText("Termine");
-		panelMeeting.add(buttonNullMeeting, "cell 0 0, alignx center");
+		buttonNullMeeting.setBorder(line);
+		panelMeeting.add(buttonNullMeeting, "cell 0 0, alignx center, w 95%!, h 170px");
 		buttonNullMeeting.setOpaque(false);
 		buttonNullMeeting.setContentAreaFilled(false);
-		buttonNullMeeting.setBorder(null);
 		try {
 			Image add = ImageIO.read(MenuBarPanelApplicant.class
 					.getResource("resources/meeting_null.png"));   
@@ -151,11 +175,19 @@ public class MenuBarPanelOverview {
 				Oberflaeche.tabBar.setSelectedIndex(4);
 			}
 		});
-		panelMeeting.add(buttonAddMeeting, "cell 0 1 ,alignx center");
-		panelMeeting.add(buttonEditMeeting, "cell 0 2 ,alignx center");
-		panelMeeting.add(buttonSearchMeeting, "cell 0 3 ,alignx center");
-		panelMeeting.add(buttonInfoMeeting, "cell 0 4 ,alignx center");
+		panelMeeting.add(buttonAddMeeting, "cell 0 1 ,alignx center, w 95%!, h 5%!");
+		panelMeeting.add(buttonEditMeeting, "cell 0 2 ,alignx center, w 95%!, h 5%!");
+		panelMeeting.add(buttonSearchMeeting, "cell 0 3 ,alignx center, w 95%!, h 5%!");
+		panelMeeting.add(buttonInfoMeeting, "cell 0 4 ,alignx center, w 95%!, h 5%!");
 		
+		buttonAddMeeting.setBorder(border);
+		buttonAddMeeting.setFont(fontButton);
+		buttonEditMeeting.setBorder(border);
+		buttonEditMeeting.setFont(fontButton);
+		buttonSearchMeeting.setBorder(border);
+		buttonSearchMeeting.setFont(fontButton);
+		buttonInfoMeeting.setBorder(border);
+		buttonInfoMeeting.setFont(fontButton);
 		
 		Oberflaeche.tabBar.addTab("Übersicht", panelOverview);
 
--- SAP_APTRA/src/git_aptra/Steuerung.java
+++ SAP_APTRA/src/git_aptra/Steuerung.java
@@ -23,7 +23,7 @@ public class Steuerung {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		new DialogLogin();
+		new Oberflaeche();
 	}	
 }
 
