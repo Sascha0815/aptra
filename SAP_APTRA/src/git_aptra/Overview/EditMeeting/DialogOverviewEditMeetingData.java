621b33f216e3f61ab8ad2b71fce3b25b4024e535
ToasterIowa13 <vollmer.johannes@web.de>
2015-06-25 10:16:42 +0200

    EditMeeting Overview Suchfunktion

--- SAP_APTRA/src/git_aptra/Overview/EditMeeting/DialogOverviewEditMeetingData.java
+++ SAP_APTRA/src/git_aptra/Overview/EditMeeting/DialogOverviewEditMeetingData.java
@@ -2,9 +2,6 @@ package git_aptra.Overview.EditMeeting;
 
 import git_aptra.Oberflaeche;
 import git_aptra.EditMeeting.DialogEditMeeting;
-import git_aptra.InfoMeeting.DialogInfoMeeting;
-import git_aptra.InfoVacancy.DialogInfoVacancy;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -31,22 +28,13 @@ public class DialogOverviewEditMeetingData {
 	private static JLabel labelInstruction = new JLabel("Meeting auswählen");
 	private static JLabel labelMeetingID = new JLabel("Termin-ID:");
 	private static JLabel labelTypMeeting = new JLabel("Art des Termins:");
-	private static JLabel labelApplicantName = new JLabel("Name des Bewerbers:");
-	private static JLabel labelApplicantFirstName = new JLabel("Vorname des Bewerbers:");
-	private static JLabel labelVacancyID = new JLabel("Arbeitsstellen-ID:");
-	private static JLabel labelPosition = new JLabel("Stellenbezeichnung:");
-	private static JLabel labelArea = new JLabel("Arbeitsbereich:");
+	private static JLabel labelLocation = new JLabel("Ort:");
 	private static JLabel labelDate = new JLabel("Datum:");
 	private static JLabel labelTime = new JLabel("Uhrzeit:");
 	
-
 	private static JTextField fieldMeetingID = new JTextField();
 	private static JTextField fieldTypMeeting = new JTextField();
-	private static JTextField fieldApplicantName = new JTextField();
-	private static JTextField fieldApplicantFirstName = new JTextField();
-	private static JTextField fieldVacancyID = new JTextField();
-	private static JTextField fieldPosition = new JTextField();
-	private static JTextField fieldArea = new JTextField();
+	private static JTextField fieldLocation = new JTextField();
 	private static JTextField fieldDate = new JTextField();
 	private static JTextField fieldTime = new JTextField();
 	
@@ -61,12 +49,9 @@ public class DialogOverviewEditMeetingData {
 		{
 			add("ID");
 			add("Bezeichnung");
-			add("Bewerber-ID");
-			add("Bewerbername");
-			add("Bewerbervorname");
-			add("Abteilung");
-			add("Arbeitsstelle");
 			add("Ort");
+			add("Zeit");
+			add("Datum");
 		}
 	};
 	
@@ -81,7 +66,16 @@ public class DialogOverviewEditMeetingData {
 	public static JTable tabledialogLoadMeetingData = new JTable(modeldialogLoadMeetingData);
 	
 	private static Font fontHeadline = new Font("Calibri", Font.BOLD, 16);
-
+	
+	private static String meetingID;
+	private static String typeMeeting;
+	private static String location;
+	private static String time;
+	private static String date;
+	@SuppressWarnings("rawtypes")
+	private static Vector resultsLoadMeetingData = new Vector();
+	@SuppressWarnings({ "rawtypes",})
+	private static Vector resultsLoadMeetingDataAll = new Vector();
 	
 	public static void loadMeetingData(){
 		
@@ -92,15 +86,10 @@ public class DialogOverviewEditMeetingData {
 				
 		fieldMeetingID.setText("");
 		fieldTypMeeting.setText("");
-		fieldApplicantName.setText("");
-		fieldApplicantFirstName.setText("");
-		fieldVacancyID.setText("");
-		fieldPosition.setText("");
-		fieldArea.setText("");
 		fieldDate.setText("");
 		fieldTime.setText("");
 		labelInstruction.setFont(fontHeadline);
-		dialogLoadMeetingData.setSize(425, 600);
+		dialogLoadMeetingData.setSize(425, 450);
 		dialogLoadMeetingData.setLocationRelativeTo(Oberflaeche.frame);
 		modeldialogLoadMeetingData.setColumnIdentifiers(COLUMN_IDENTIFIERS_OVERWIEV_MEETING_INFO);
 		tabledialogLoadMeetingData.getTableHeader().setReorderingAllowed(false);
@@ -111,39 +100,38 @@ public class DialogOverviewEditMeetingData {
 		scrollPaneLoadMeetingData.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		tabledialogLoadMeetingData.setRowHeight(20);
 		tabledialogLoadMeetingData.setAutoCreateRowSorter(true);
-		
-		paneldialogLoadMeetingData.setLayout(new MigLayout("", "[grow,left][grow,right]", "[][][][][][][][][][][][][][][][][][][][][]push[]"));
+		resultsLoadMeetingDataAll = LoadMeetingData.insertLoadMeetingDataIntoTable();
+		modeldialogLoadMeetingData.setDataVector(resultsLoadMeetingDataAll,COLUMN_IDENTIFIERS_OVERWIEV_MEETING_INFO);
+		modeldialogLoadMeetingData.fireTableDataChanged();
+		paneldialogLoadMeetingData.setLayout(new MigLayout("", "[grow,left][grow,right]", "[][][][][][][][][][]5[]5[]5[]"));
 		paneldialogLoadMeetingData.add(labelInstruction,"cell 0 0 2 1,alignx center");
 		paneldialogLoadMeetingData.add(labelMeetingID,"cell 0 2,alignx left");
 		paneldialogLoadMeetingData.add(fieldMeetingID,"cell 0 3 2 1,growx");
 		paneldialogLoadMeetingData.add(labelTypMeeting,"cell 0 4,alignx left");
 		paneldialogLoadMeetingData.add(fieldTypMeeting,"cell 0 5 2 1,growx");
-		paneldialogLoadMeetingData.add(labelApplicantName,"cell 0 6,alignx left");
-		paneldialogLoadMeetingData.add(fieldApplicantName,"cell 0 7 2 1,growx");
-		paneldialogLoadMeetingData.add(labelApplicantFirstName,"cell 0 8,alignx left");
-		paneldialogLoadMeetingData.add(fieldApplicantFirstName,"cell 0 9 2 1,growx");
-		paneldialogLoadMeetingData.add(labelVacancyID,"cell 0 10,alignx left");
-		paneldialogLoadMeetingData.add(fieldVacancyID,"cell 0 11 2 1,growx");
-		
-		paneldialogLoadMeetingData.add(labelPosition,"cell 0 12 2 1,growx");
-		paneldialogLoadMeetingData.add(fieldPosition,"cell 0 13 2 1,growx");
-		paneldialogLoadMeetingData.add(labelArea,"cell 0 14 2 1,growx");
-		paneldialogLoadMeetingData.add(fieldArea,"cell 0 15 2 1,growx");
-		paneldialogLoadMeetingData.add(labelDate,"cell 0 16 2 1,growx");
-		paneldialogLoadMeetingData.add(fieldDate,"cell 0 17 2 1,growx");
-		paneldialogLoadMeetingData.add(labelTime,"cell 0 18 2 1,growx");
-		paneldialogLoadMeetingData.add(fieldTime,"cell 0 19 2 1,growx");
-		
-		paneldialogLoadMeetingData.add(buttonSearch, "cell 1 20, alignx right");
-		paneldialogLoadMeetingData.add(scrollPaneLoadMeetingData, "cell 0 21 2 1,growx");
-		paneldialogLoadMeetingData.add(buttonAbort,"cell 0 22,alignx left");
-		paneldialogLoadMeetingData.add(buttonSave,"cell 1 22,alignx right");
+		paneldialogLoadMeetingData.add(labelLocation,"cell 0 6,alignx left");
+		paneldialogLoadMeetingData.add(fieldLocation,"cell 0 7 2 1,growx");
+		paneldialogLoadMeetingData.add(labelDate,"cell 0 8 2 1,growx");
+		paneldialogLoadMeetingData.add(fieldDate,"cell 0 9 2 1,growx");
+		paneldialogLoadMeetingData.add(labelTime,"cell 0 10 2 1,growx");
+		paneldialogLoadMeetingData.add(fieldTime,"cell 0 11 2 1,growx");	
+		paneldialogLoadMeetingData.add(buttonSearch, "cell 1 12, alignx right");
+		paneldialogLoadMeetingData.add(scrollPaneLoadMeetingData, "cell 0 13 2 1,growx");
+		paneldialogLoadMeetingData.add(buttonAbort,"cell 0 14,alignx left");
+		paneldialogLoadMeetingData.add(buttonSave,"cell 1 14,alignx right");
 
 		
 		buttonSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-			
-			
+				getMeetingData();
+				try {
+					resultsLoadMeetingData = LoadMeetingData.loadMeetingData();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				modeldialogLoadMeetingData.setDataVector(resultsLoadMeetingData,COLUMN_IDENTIFIERS_OVERWIEV_MEETING_INFO);
+				modeldialogLoadMeetingData.fireTableDataChanged();
 			}
 		});
 		
@@ -163,7 +151,48 @@ public class DialogOverviewEditMeetingData {
 		dialogLoadMeetingData.setVisible(true);
 	}
 	
-
+	public static void getMeetingData(){
+		try {
+			meetingID = fieldMeetingID.getText();
+		} catch (Exception e) {
+		}
+		try {
+			typeMeeting = fieldTypMeeting.getText();
+		} catch (Exception e) {
+		}
+		try {
+			location = fieldLocation.getText();
+		} catch (Exception e) {
+		}
+		try {
+			time = fieldTime.getText();
+		} catch (Exception e) {
+		}
+		try {
+			date = fieldDate.getText();
+		} catch (Exception e) {
+		}
+	}
 	
+	public static String getMeetingID() {
+		return meetingID;
+	}
+	
+	public static String getTypeMeeting() {
+		return typeMeeting;
+	}
+
+	public static String getLocation() {
+		return location;
+	}
+	
+	public static String getTime() {
+		return time;
+	}
+
+	public static String getDate() {
+		return date;
+	}
+
 }
 
---
+++ SAP_APTRA/src/git_aptra/Overview/EditMeeting/LoadMeetingData.java
@@ -0,0 +1,135 @@
+package git_aptra.Overview.EditMeeting;
+
+import git_aptra.Login.Login;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
+public class LoadMeetingData {
+	
+	private static String meetingID;
+	private static String typeMeeting;
+	private static String location;
+	private static String time;
+	private static String date;
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static Vector loadMeetingData() throws SQLException {
+		Connection dbConnection = null;
+		boolean first = true;
+		Vector resultsLoadMeetingData = new Vector();
+		if (!(DialogOverviewEditMeetingData.getMeetingID().equals(""))) {
+			if (first == true) {
+				meetingID = "where meetingID = '"
+						+ DialogOverviewEditMeetingData.getMeetingID() + "'";
+				first = false;
+			} else {
+				meetingID = " AND meetingID = '"
+						+ DialogOverviewEditMeetingData.getMeetingID() + "'";
+			}
+		} else {
+			meetingID = "";
+		}
+		
+		if (!DialogOverviewEditMeetingData.getTypeMeeting().equals("")) {
+			if (first == true) {
+				typeMeeting = "where typeMeeting = '"
+						+ DialogOverviewEditMeetingData.getTypeMeeting() + "'";
+				first = false;
+			} else {
+				typeMeeting = " AND typeMeeting = '"
+						+ DialogOverviewEditMeetingData.getTypeMeeting() + "'";
+			}
+		} else {
+			typeMeeting = "";
+		}
+
+		if (!DialogOverviewEditMeetingData.getLocation().equals("")) {
+			if (first == true) {
+				location = "where location = '"
+						+ DialogOverviewEditMeetingData.getLocation() + "'";
+				first = false;
+			} else {
+				location = " AND location = '"
+						+ DialogOverviewEditMeetingData.getLocation() + "'";
+			}
+		} else {
+			location = "";
+		}
+	
+		if (!DialogOverviewEditMeetingData.getTime().equals("")) {
+			if (first == true) {
+				time = "where time = '"
+						+ DialogOverviewEditMeetingData.getTime() + "'";
+				first = false;
+			} else {
+				time = " AND time = '"
+						+ DialogOverviewEditMeetingData.getTime() + "'";
+			}
+		} else {
+			time = "";
+		}
+		if (!DialogOverviewEditMeetingData.getDate().equals("")) {
+			if (first == true) {
+				date = "where date = '"
+						+ DialogOverviewEditMeetingData.getDate() + "'";
+				first = false;
+			} else {
+				date = " AND date = '"
+						+ DialogOverviewEditMeetingData.getDate() + "'";
+			}
+		} else {
+			date = "";
+		}
+		
+		try {
+			dbConnection = Login.getConnection();
+
+			Statement stmt = dbConnection.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from meeting " + meetingID + typeMeeting
+					+ location + time + date);
+		
+			while (rs.next()) {
+				Vector meeting = new Vector();
+				meeting.add(rs.getString(3));
+				meeting.add(rs.getString(7));
+				meeting.add(rs.getString(8));
+				meeting.add(rs.getString(10));
+				meeting.add(rs.getString(9));
+				resultsLoadMeetingData.add(meeting);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Datenbank - loadVacancyData" + e.getMessage());
+		}
+		return resultsLoadMeetingData;	
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Vector insertLoadMeetingDataIntoTable(){
+		Vector resultsLoadMeetingDataAll = new Vector();
+		try {
+			Connection dbConnection = null;
+			dbConnection = Login.getConnection();
+			
+			Statement stmt = dbConnection.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from meeting");
+		
+			while (rs.next()) {
+				Vector meeting = new Vector();
+				meeting.add(rs.getString(3));
+				meeting.add(rs.getString(7));
+				meeting.add(rs.getString(8));
+				meeting.add(rs.getString(10));
+				meeting.add(rs.getString(9));
+				resultsLoadMeetingDataAll.add(meeting);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Datenbank - loadvacancyDataAll" + e.getMessage());
+		}
+		return resultsLoadMeetingDataAll;
+	}
+}
